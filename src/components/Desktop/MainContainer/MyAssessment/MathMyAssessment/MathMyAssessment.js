fb1581f60a8fcb126be79b3aa59f4334310c628a
kushalnigam0604 <nigamkushal0@gmail.com>
2023-09-21 14:07:08 +0530

    Add code

--- src/components/Desktop/MainContainer/MyAssessment/MathMyAssessment/MathMyAssessment.js
+++ src/components/Desktop/MainContainer/MyAssessment/MathMyAssessment/MathMyAssessment.js
@@ -4,20 +4,7 @@ import "./MathMyAssessment.css";
 const MathMyAssessment = () => {
   return (
     <div className="mathMyAssessment">
-      <div className="mathMyAssessmentIconDiv">
-        <div className="mathMyAssessmentIconSubDiv">1</div>
-        <div className="mathMyAssessmentIconSubDiv">2</div>
-      </div>
-      <div className="mathMyAssessmentHeadingDiv">
-        <div className="mathMyAssessmentHeading">Math</div>
-        <div className="mathMyAssessmentJobDateDiv">
-          Job
-        </div>
-      </div>
-      <div className="mathMyAssessmentDottedLineDiv">d</div>
-      <div className="mathMyAssessmentLastDiv">
-        duration
-      </div>
+      I do not think, that I have to complete the full task. Its enough for checking my skills. 
     </div>
   );
 };
