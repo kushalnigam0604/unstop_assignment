7aaf5e7cc1d70e8017d1171863d760bf9cfc26c1
kushalnigam0604 <nigamkushal0@gmail.com>
2023-09-21 13:53:28 +0530

    Add code

--- src/components/Desktop/MainContainer/MyAssessment/MathMyAssessment/MathMyAssessment.js
+++ src/components/Desktop/MainContainer/MyAssessment/MathMyAssessment/MathMyAssessment.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import './MathMyAssessment.css';
+import React from "react";
+import "./MathMyAssessment.css";
 
 const MathMyAssessment = () => {
   return (
-    <div className='mathMyAssessment'>
-      math
+    <div className="mathMyAssessment">
+      <div className="mathMyAssessmentIconDiv">
+        <div className="mathMyAssessmentIconSubDiv">1</div>
+        <div className="mathMyAssessmentIconSubDiv">2</div>
+      </div>
+      <div className="mathMyAssessmentHeadingDiv">
+        <div className="mathMyAssessmentHeading">Math</div>
+        <div className="mathMyAssessmentJobDateDiv">
+          Job
+        </div>
+      </div>
+      <div className="mathMyAssessmentDottedLineDiv">d</div>
+      <div className="mathMyAssessmentLastDiv">
+        duration
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MathMyAssessment
+export default MathMyAssessment;
